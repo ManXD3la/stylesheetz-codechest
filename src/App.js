fc39361cad17836badb18b188de1853e2d9fdc3c
M Jones <jonesman2800@gmail.com>
2021-05-03 18:19:31 -0400

    init blossom page first page!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import NeonBlossomPage from './pages/NeonBlossom/NeonBlossomPage';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+        </header>
+        <Switch>
+          <Route exact path='/p1' component={NeonBlossomPage} />
+        </Switch>
+      </main>
+    );
+  }
+};
 
 export default App;
---
+++ src/pages/NeonBlossom/NeonBlossomPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './NeonBlossomPage.css';
+
+function NeonBlossomPage() {
+        return (
+            <div className='container'>
+                
+            </ div>
+        )
+};
+
+export default NeonBlossomPage;
