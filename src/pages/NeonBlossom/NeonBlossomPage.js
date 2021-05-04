e2ebb4f5e2a8e0e48ab7c7d89c4a0fb02eb7128e
M Jones <jonesman2800@gmail.com>
2021-05-03 20:48:14 -0400

    page structure set

--- src/pages/NeonBlossom/NeonBlossomPage.js
+++ src/pages/NeonBlossom/NeonBlossomPage.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 import './NeonBlossomPage.css';
+
+import originalBlossom from '../../assets/images/cherry-blossom-cropped.jpg';
+import editedBlossom from '../../assets/images/cherry-blossom-phytoblue-cropped.jpg'
 
 function NeonBlossomPage() {
         return (
-            <div className='container'>
-                
+            <div className='container' id="moz-reflect">
+                <div className='neon-btn-container'>
+                    <Link clasname='neon-button' href='/p1' >
+                        NEON
+                    </Link>
+
+                </div>
+                <img classname='reflect-left'src={editedBlossom}/>
             </ div>
         )
 };
