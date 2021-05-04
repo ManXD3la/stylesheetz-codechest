82380089b2e94ef6cb5287180cf83a0861f1cf0f
M Jones <jonesman2800@gmail.com>
2021-05-04 12:42:06 -0400

    dateTab displayed over app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import DateTab from './components/DateTab/DateTab';
 import NeonBlossomPage from './pages/NeonBlossom/NeonBlossomPage';
 
 class App extends Component {
   render() {
     return (
       <main className="App">
+        <DateTab />
         <Switch>
           <Route path='/' component={NeonBlossomPage} />
         </Switch>
