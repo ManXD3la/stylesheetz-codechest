c2cb6af690d1bdc31118aea6a4329a02c7c92091
M Jones <jonesman2800@gmail.com>
2021-05-16 16:02:29 -0400

    init form page with basic styling

---
+++ src/pages/FutureFeedbackForm/FutureFeedbackFormPage.js
@@ -0,0 +1,34 @@
+/* As practice, I'm beginning to lay out most lifecycle methods in order keep in imind how componenet lifecycles work. Hope this this helpful to you as it has been to me.*/
+
+import React from "react";
+import "./FutureFeedbackFormPage.css";
+
+export default class FutureFeedbackFormPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            creationDate: new Date('05/15/2021'),
+            formSubmissions: []
+        }
+    }
+
+    // Life Cycles
+    componentDidMount() {}
+
+    
+    shouldComponentUpdate() {}
+    
+    componentDidUpdate() {}
+    
+    componentDidCatch() {}
+    
+    render() {
+        return(
+            <div className="container">
+                hello universe
+            </div>
+        )
+    }
+
+    componentWillUnmount() {}
+}
